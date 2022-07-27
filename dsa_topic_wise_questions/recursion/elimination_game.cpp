c8715faa9a255646bdf4912114e04566ee9c498c
Atul Dwivedi <atultechd@gmail.com>
2022-07-27 23:00:02 +0530

    1st solution

---
+++ dsa_topic_wise_questions/recursion/elimination_game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+
+public:
+
+    int lastRemaining(int n) {
+
+        if(n == 1) return 1;
+
+        if(n <= 4) return 2;
+
+        if(n % 2 != 0) n = n-1;
+
+        if(n % 4 != 0) return 4 * lastRemaining(n/4);
+        
+        return 4 * lastRemaining(n/4) - 2;
+    }
+};
