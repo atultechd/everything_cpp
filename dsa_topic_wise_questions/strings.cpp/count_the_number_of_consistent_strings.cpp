ee15b919f22ccf71d8d19e636b2962b4afb54aee
Atul Dwivedi <atultechd@gmail.com>
2022-08-20 00:31:30 +0530

    using bool array

--- dsa_topic_wise_questions/strings.cpp/count_the_number_of_consistent_strings.cpp
+++ dsa_topic_wise_questions/strings.cpp/count_the_number_of_consistent_strings.cpp
@@ -1,13 +1,28 @@
 class Solution {
+
 public:
+
     int countConsistentStrings(string allowed, vector<string>& words) {
+
         int res = words.size();
-        bool alpha[26] = {};
-        for (char c: allowed) alpha[c - 'a'] = true;
-        for (string word: words) {
-            for (char c: word) if (!alpha[c - 'a']) {
-                res--;
-                break;
+
+        bool alphabet[26] = {false};
+
+        for(char c : allowed){
+
+            alphabet[c-'a'] = true;
+        }
+        
+        for(auto word : words){
+
+            for(auto c : word){
+
+                if(alphabet[c-'a'] == false){
+
+                    res--;
+                    
+                    break;
+                }
             }
         }
         return res;
