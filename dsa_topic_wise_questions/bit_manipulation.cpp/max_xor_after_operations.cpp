190b9bbc729c7a847364bcd91ce733f8ac9d049f
Atul Dwivedi <atultechd@gmail.com>
2022-08-22 23:56:40 +0530

    using bitwise or

---
+++ dsa_topic_wise_questions/bit_manipulation.cpp/max_xor_after_operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+
+public:
+
+    int maximumXOR(vector<int>& nums) {
+
+        int res = 0;
+
+        for(auto n : nums){
+
+            res |= n;
+        }
+        
+        return res;
+    }
+};
