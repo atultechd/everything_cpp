b62b1a15e6b3f531ae144a2c364dda6816e629be
Atul Dwivedi <atultechd@gmail.com>
2022-08-27 21:33:13 +0530

    using unordered_set

---
+++ dsa_topic_wise_questions/matrix.cpp/image.cpp
@@ -0,0 +1,47 @@
+// codeforces contest..........(accepted)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while(t--){
+
+        char c[2][2];
+
+        for(int i=0; i<2; i++){
+
+            for(int j=0; j<2; j++){
+
+                cin >> c[i][j];
+            }
+            cout << endl;
+        }
+        unordered_set<char> u_set;
+
+        for(int i=0; i<2; i++){
+
+            for(int j=0; j<2; j++){
+
+                if(u_set.find(c[i][j]) == u_set.end()){
+
+                    u_set.insert(c[i][j]);
+                }
+            }
+        }
+        if(u_set.size()==1) cout << 0 << endl;
+
+        if(u_set.size()==2) cout << 1 << endl;
+
+        if(u_set.size()==3) cout << 2 << endl;
+
+        if(u_set.size()==4) cout << 3 << endl;
+        
+    } 
+
+    return 0;
+}
