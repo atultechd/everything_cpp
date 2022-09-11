e3c1a42aa32cc9f381d07ce188a4feeed1623eb2
Atul Dwivedi <atultechd@gmail.com>
2022-09-11 22:34:31 +0530

    using extra space

---
+++ dsa_topic_wise_questions/math.cpp/concatenation_of_array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+
+public:
+
+    vector<int> getConcatenation(vector<int>& nums) {
+
+        vector<int> res(nums.begin(), nums.end());
+
+        for(int i=0; i<nums.size(); i++){
+
+            res.push_back(nums[i]);
+        }
+        return res;
+    }
+};
+
+// ....................................................................
+
+class Solution {
+
+public:
+
+    vector<int> getConcatenation(vector<int>& nums) {
+
+        int n= nums.size();
+
+        for(int i=0; i<n; i++){
+          
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
