316cfcec07c4f8272a4badcb4f9d592d563b774f
Atul Dwivedi <atultechd@gmail.com>
2022-09-28 23:59:07 +0530

    using extra space

--- dsa_topic_wise_questions/array_questions.cpp/group_the_people_given_the_group_size_they_belong.cpp
+++ dsa_topic_wise_questions/array_questions.cpp/group_the_people_given_the_group_size_they_belong.cpp
@@ -1,11 +1,17 @@
 vector<vector<int>> groupThePeople(vector<int>& gz) {
-  vector<vector<int>> res, groups(gz.size() + 1);
+
+  vector<vector<int>> ans, groups(gz.size() + 1);
+
   for (auto i = 0; i < gz.size(); ++i) {
+
     groups[gz[i]].push_back(i);
+
     if (groups[gz[i]].size() == gz[i]) {
-      res.push_back({});
-      swap(res.back(), groups[gz[i]]);
+
+      ans.push_back({});
+      
+      swap(ans.back(), groups[gz[i]]);
     }
   }
-  return res;
+  return ans;
 }
