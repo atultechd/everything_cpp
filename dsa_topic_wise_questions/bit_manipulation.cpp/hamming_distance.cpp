b74e2c7be72275c3286126e70230750a60b152d7
Atul Dwivedi <atultechd@gmail.com>
2022-10-07 20:01:02 +0530

    using both bitwise XOR and AND

--- dsa_topic_wise_questions/bit_manipulation.cpp/hamming_distance.cpp
+++ dsa_topic_wise_questions/bit_manipulation.cpp/hamming_distance.cpp
@@ -11,9 +11,33 @@ public:
         while(a > 0){
 
             a = a & (a-1);
-            
+
             h_distance++;
         }
         return h_distance;
     }
 };
+
+// ................................................................
+
+
+class Solution {
+
+public:
+
+    int hammingDistance(int x, int y){
+
+        int h_distance = 0;
+
+        int a = x^y;
+
+        while(a>0){
+
+            h_distance += a & 1;
+
+            a >>= 1;
+        }
+
+        return h_distance;
+    }
+}
