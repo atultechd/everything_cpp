095fcbfd88355a0ff6e244da411af866533a4a4e
Atul Dwivedi <atultechd@gmail.com>
2022-10-08 16:41:08 +0530

    using low high pointer

--- dsa_topic_wise_questions/array_questions.cpp/minimum_no_of_operations_to_move_all_balls_to_each_box.cpp
+++ dsa_topic_wise_questions/array_questions.cpp/minimum_no_of_operations_to_move_all_balls_to_each_box.cpp
@@ -36,3 +36,34 @@ public:
         return res;
     }
 };
+
+class Solution {
+
+public:
+
+    vector<int> minOperations(string boxes) {
+
+        vector<int> res(boxes.size());
+
+        // from left to right;
+        for(int i=0,count=0,operations=0; i<boxes.size(); i++){
+
+            res[i] += operations;
+
+            count += boxes[i] == '1' ? 1 : 0;
+
+            operations +=  count;
+        }
+
+        // from right to left;
+        for(int i=boxes.size()-1,count=0,operations=0; i>=0; i--){
+
+            res[i] += operations;
+
+            count += boxes[i] == '1' ? 1 : 0;
+            
+            operations +=  count;
+        }
+        return res;
+    }
+};
