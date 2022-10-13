609a24427a120596ac657f2796ca357f587a7b14
Atul Dwivedi <atultechd@gmail.com>
2022-10-13 18:19:16 +0530

    using two pointers without extra space

--- dsa_topic_wise_questions/array_questions.cpp/rearrange_array_elements_by_sign.cpp
+++ dsa_topic_wise_questions/array_questions.cpp/rearrange_array_elements_by_sign.cpp
@@ -19,8 +19,40 @@ public:
         for(int i=0; i<nums.size()/2; i++){
 
             res.push_back(pos[i]);
-            
+
             res.push_back(neg[i]);
+        }
+        return res;
+    }
+};
+
+// ..................................................................
+
+class Solution {
+
+public:
+
+    vector<int> rearrangeArray(vector<int>& nums) {
+
+        vector<int> res(nums.size(),0);
+
+        int pos = 0, neg = 1;
+
+        for(auto num : nums){
+
+            if(num > 0){
+
+                res[pos] = num;
+
+                pos += 2;
+            }
+
+            else if(num < 0){
+
+                res[neg] = num;
+                
+                neg += 2;
+            }
         }
         return res;
     }
