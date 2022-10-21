eb49934500cc7ce623ac62019f241f89e831a38f
Atul Dwivedi <atultechd@gmail.com>
2022-10-21 21:45:21 +0530

    using extra space

--- dsa_topic_wise_questions/array_questions.cpp/how_many_numbers_are_smaller_than_the_current_number.cpp
+++ dsa_topic_wise_questions/array_questions.cpp/how_many_numbers_are_smaller_than_the_current_number.cpp
@@ -11,10 +11,42 @@ public:
             int count = 0;
 
             for(int j=0; j<nums.size(); j++){
-              
+
                 if(nums[j] < nums[i] && j!= i) count++;
             }
             res.push_back(count);
+        }
+        return res;
+    }
+};
+
+// ............................................................................
+
+class Solution {
+
+public:
+
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+
+        vector<int> res;
+        
+        int count[101] = {0};
+
+        for(int i=0; i<nums.size(); i++){
+
+            count[nums[i]]++;
+        }
+
+        for(int i=1; i<101; i++){
+
+            count[i] += count[i-1];
+        }
+
+        for(int i=0; i<nums.size(); i++){
+
+            if(nums[i] == 0) res.push_back(0);
+            
+            else res.push_back(count[nums[i]-1]);
         }
         return res;
     }
