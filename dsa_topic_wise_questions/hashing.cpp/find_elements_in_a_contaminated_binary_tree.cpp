20fd685f57a7e8ba9186279e175c86d2be76b461
Atul Dwivedi <atultechd@gmail.com>
2022-10-27 23:28:15 +0530

    using unordered_set and recursion

---
+++ dsa_topic_wise_questions/hashing.cpp/find_elements_in_a_contaminated_binary_tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+
+    unordered_set<int> u_set;
+
+public:
+
+    void recover_bt(TreeNode* root, int x){
+
+        if(!root) return;
+
+        root->val = x;
+
+        u_set.emplace(x);
+
+        recover_bt(root->left, 2*x + 1);
+
+        recover_bt(root->right, 2*x + 2);
+    }
+
+    FindElements(TreeNode* root) {
+
+        recover_bt(root, 0);
+    }
+    
+    
+    bool find(int target) {
+      
+        return u_set.count(target);
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
