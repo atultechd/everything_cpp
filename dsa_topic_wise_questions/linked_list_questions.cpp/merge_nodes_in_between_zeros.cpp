fc54cc7c2e430d9ca0444253e221725a03abbe1a
Atul Dwivedi <atultechd@gmail.com>
2022-11-05 23:59:22 +0530

    using recursion

---
+++ dsa_topic_wise_questions/linked_list_questions.cpp/merge_nodes_in_between_zeros.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        if(!head->next) return NULL;
+        ListNode *temp = head->next;
+        int count = 0;
+        while(temp->val != 0){
+            count += temp->val;
+            temp =temp->next;
+        }
+        head->next->val = count;
+        head->next->next = mergeNodes(temp);
+        return head->next;
+    }
+};
