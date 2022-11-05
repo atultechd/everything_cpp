b5e6b5a447b31340ce7d1708d10f9c3b9ef7eda2
Atul Dwivedi <atultechd@gmail.com>
2022-11-06 00:07:59 +0530

    using recursion

--- dsa_topic_wise_questions/linked_list_questions.cpp/merge_nodes_in_between_zeros.cpp
+++ dsa_topic_wise_questions/linked_list_questions.cpp/merge_nodes_in_between_zeros.cpp
@@ -9,17 +9,28 @@
  * };
  */
 class Solution {
+
 public:
+
     ListNode* mergeNodes(ListNode* head) {
+
         if(!head->next) return NULL;
+
         ListNode *temp = head->next;
+
         int count = 0;
+
         while(temp->val != 0){
+
             count += temp->val;
+
             temp =temp->next;
         }
+
         head->next->val = count;
+
         head->next->next = mergeNodes(temp);
+        
         return head->next;
     }
 };
