dfc6b32e6d43486995aaa99d749270870c11ae74
Atul Dwivedi <atultechd@gmail.com>
2022-11-07 16:26:37 +0530

    using bit manipulation

--- dsa_topic_wise_questions/bit_manipulation.cpp/score_after_flipping_matrix.cpp
+++ dsa_topic_wise_questions/bit_manipulation.cpp/score_after_flipping_matrix.cpp
@@ -1,47 +1,52 @@
 class Solution {
+
 public:
-    int matrixScore(vector<vector<int>>& A) {
-        int m = A.size();
-        int n = A[0].size();
-        // if the leading bit is 0 --> toggle it
-        for(int i = 0; i < m; ++i) {
-            if(A[i][0] == 0) {
-                for(int j = 0; j < n; ++j) {
-                    A[i][j] ^= 1;
-                }
-            }
-        }
-        // process each col (except leading col)
-        // if the numbers of 1 is larger than m/2 (where m is the row nums), toggle it.
-        for(int i = 1; i < n; ++i) {
-            int cnt = 0;
-            for(int j = 0; j < m; ++j) {
-                cnt += A[j][i];
-            }            
-            if(cnt <= (m>>1)) {
-                for(int j = 0; j < m; ++j) {
-                    A[j][i] ^= 1;
+
+    int matrixScore(vector<vector<int>>& grid) {
+
+        int row = grid.size();
+
+        int col = grid[0].size();
+
+        for(int i=0; i<row; i++){
+
+            if(grid[i][0] == 0){
+
+                for(int j=0; j<col; j++){
+
+                    grid[i][j] ^= 1;
                 }
             }
         }
         
-        // calculate the result
-        int res = 0;
-        int bin = 1;
-        for(int j = n-1; j >= 0; --j) {
-            for(int i = 0; i < m; ++i) {
-                res += A[i][j]*bin;
+        for(int i=1; i<col; i++){
+
+            int count = 0;
+
+            for(int j=0; j<row; j++){
+
+                count += grid[j][i];
             }
-            bin = (bin << 1);
+            
+            if(count <= (row>>1)){
+
+                for(int j=0; j<row; j++){
+
+                    grid[j][i] ^= 1;
+                }
+            }
         }
-        /*
-        for(auto i:A) {
-            for(auto j:i)
-                cout << j << " ";
-            cout << endl;
+
+        int res = 0, binary = 1;
+
+        for(int i=col-1; i>=0; i--){
+
+            for(int j=0; j<row; j++){
+                
+                res += grid[j][i] * binary;
+            }
+            binary = (binary<<1);
         }
-        */
-        
         return res;
     }
 };
