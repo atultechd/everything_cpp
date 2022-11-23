24ae2d34d7419bc1e31656e65ed243ca87525449
Atul Dwivedi <atultechd@gmail.com>
2022-11-24 00:11:37 +0530

    using slow fast pointer

--- dsa_topic_wise_questions/binary_search_tree.cpp/convert_sorted_list_to_bst.cpp
+++ dsa_topic_wise_questions/binary_search_tree.cpp/convert_sorted_list_to_bst.cpp
@@ -1,16 +1,54 @@
-TreeNode* sortedListToBST(ListNode* head) {
-    if (!head) return nullptr;
-    if (!head->next) return new TreeNode(head->val);
-    auto fast = head->next, slow = head;
-    while (fast->next && fast->next->next) {
-        fast = fast->next->next;
-        slow = slow->next;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+public:
+
+    TreeNode* sortedListToBST(ListNode* head) {
+
+        if(!head) return NULL;
+
+        if(!head->next) return new TreeNode(head->val);
+
+        auto slow = head;
+        auto fast = head->next;
+
+        while(fast->next && fast->next->next){
+
+            slow = slow->next;
+
+            fast = fast->next->next;
+        }
+
+        auto mid = slow->next;
+
+        slow->next = NULL;
+
+        TreeNode *root = new TreeNode(mid->val);
+
+        root->left = sortedListToBST(head);
+
+        root->right = sortedListToBST(mid->next);
+
+        return root;
     }
-    
-    auto mid = slow->next;
-    slow->next = nullptr;
-    auto root = new TreeNode(mid->val);
-    root->left = sortedListToBST(head);
-    root->right = sortedListToBST(mid->next);
-    return root;
-}
+};
